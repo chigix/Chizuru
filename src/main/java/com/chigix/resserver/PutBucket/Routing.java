4584794936277aa8218283cf0d326a2da977d204
Richard Lea <chigix@zoho.com>
2017-01-11 20:39:42 +0900

    new: usr: API for PUT_BUCKET.
    Signed-off-by: Richard Lea <chigix@zoho.com>

---
+++ src/main/java/com/chigix/resserver/PutBucket/Routing.java
@@ -0,0 +1,53 @@
+package com.chigix.resserver.PutBucket;
+
+import com.chigix.resserver.ApplicationContext;
+import com.chigix.resserver.entity.Bucket;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.router.HttpRouted;
+import io.netty.handler.codec.http.router.RoutingConfig;
+import io.netty.handler.routing.DefaultExceptionForwarder;
+
+/**
+ *
+ * @author Richard Lea <chigix@zoho.com>
+ */
+public class Routing extends RoutingConfig.PUT {
+
+    private final ApplicationContext application;
+
+    public Routing(ApplicationContext application) {
+        this.application = application;
+    }
+
+    @Override
+    public String configureRoutingName() {
+        return "PUT_BUCKET";
+    }
+
+    @Override
+    public String configurePath() {
+        return "/:bucketName";
+    }
+
+    @Override
+    public void configurePipeline(ChannelPipeline pipeline) {
+        pipeline.addLast(new SimpleChannelInboundHandler<HttpRouted>() {
+            @Override
+            protected void messageReceived(ChannelHandlerContext ctx, HttpRouted msg) throws Exception {
+                msg.allow();
+                Bucket bucket = application.BucketDao.createBucket((String) msg.decodedParams().get("bucketName"));
+                DefaultFullHttpResponse resp = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+                resp.headers().add(HttpHeaderNames.LOCATION, "/" + bucket.getName());
+                ctx.writeAndFlush(resp);
+            }
+
+        }).addLast(new DefaultExceptionForwarder());
+    }
+
+}
