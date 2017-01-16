f1112da45690f2b98da77cb5426066cdadc05784
Richard Lea <chigix@zoho.com>
2017-01-17 02:02:34 +0900

    new: dev: Common chunk entity class provided.
    Signed-off-by: Richard Lea <chigix@zoho.com>

---
+++ src/main/java/com/chigix/resserver/entity/Chunk.java
@@ -0,0 +1,48 @@
+package com.chigix.resserver.entity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author Richard Lea <chigix@zoho.com>
+ */
+public class Chunk {
+
+    private final String contentHash;
+
+    public Chunk(String contentHash) {
+        this.contentHash = contentHash;
+    }
+
+    public InputStream getInputStream() throws IOException {
+        return new InputStream() {
+            @Override
+            public int read() throws IOException {
+                return -1;
+            }
+        };
+    }
+
+    public String getContentHash() {
+        return contentHash;
+    }
+
+    /**
+     * SHA-256 digest which could be compared with the `X-Amz-Content-Sha256`
+     *
+     * @return
+     */
+    public static MessageDigest contentHashDigest() {
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException(ex);
+        }
+        return digest;
+    }
+
+}
