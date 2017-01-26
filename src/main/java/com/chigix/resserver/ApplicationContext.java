5bd8581226ecab35de073d32b27b2ca4e7cd33ac
Richard Lea <chigix@zoho.com>
2017-01-27 03:15:49 +0900

    new: dev: dao instance in application context.
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- src/main/java/com/chigix/resserver/ApplicationContext.java
+++ src/main/java/com/chigix/resserver/ApplicationContext.java
@@ -1,6 +1,18 @@
 package com.chigix.resserver;
 
+import com.chigix.resserver.entity.Chunk;
+import com.chigix.resserver.entity.dao.BucketDao;
+import com.chigix.resserver.entity.dao.ResourceDao;
+import com.chigix.resserver.mapdbimpl.dao.BucketKeys;
+import com.chigix.resserver.mapdbimpl.dao.ResourceKeys;
+import com.chigix.resserver.mapdbimpl.dao.BucketDaoImpl;
+import com.chigix.resserver.mapdbimpl.dao.ChunkDaoImpl;
+import com.chigix.resserver.mapdbimpl.dao.ChunkKeys;
+import com.chigix.resserver.mapdbimpl.dao.ResourceDaoImpl;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import org.joda.time.DateTime;
@@ -12,7 +24,11 @@ import org.mapdb.DB;
  */
 public class ApplicationContext {
 
+    private final String requestIdHeaderName = Long.toHexString(Double.doubleToLongBits(Math.random()));
+
     private final Map<String, String> nodesMapping;
+
+    private final int maxChunkSize;
 
     private final File chunksDir;
 
@@ -20,34 +36,63 @@ public class ApplicationContext {
 
     private final String currentNodeId;
 
-    private final String bucketName;
-
     private final DateTime creationDate;
 
-    public ApplicationContext(String currentNodeId, String bucketName, DateTime creationDate, File dataDir, DB db) {
+    public final BucketDao BucketDao;
+
+    public final ResourceDao ResourceDao;
+
+    public final ChunkDaoImpl ChunkDao;
+
+    public ApplicationContext(String currentNodeId, DateTime creationDate, int maxChunkSize, File dataDir, DB db) {
         this.currentNodeId = currentNodeId;
         this.chunksDir = dataDir;
+        this.maxChunkSize = maxChunkSize;
         this.db = db;
         this.nodesMapping = new HashMap<>();
         this.nodesMapping.put(currentNodeId, "127.0.0.1");
-        this.bucketName = bucketName;
         this.creationDate = creationDate;
+        BucketDao = new BucketDaoImpl(db);
+        ResourceDao = new ResourceDaoImpl(db);
+        ChunkDao = new ChunkDaoImpl(db) {
+            @Override
+            public Chunk newChunk(String contentHash, int chunk_size) {
+                return new Chunk(contentHash, chunk_size) {
+                    @Override
+                    public InputStream getInputStream() throws IOException {
+                        return new FileInputStream(new File(chunksDir, this.getContentHash()));
+                    }
+
+                };
+            }
+        };
+        ((ResourceDaoImpl) ResourceDao).assembleDaos((BucketDaoImpl) BucketDao, ChunkDao);
     }
 
-    public DB getDb() {
-        return db;
+    public String getRequestIdHeaderName() {
+        return requestIdHeaderName;
+    }
+
+    public void updateDBScheme() {
+        BucketKeys.updateDBScheme(db);
+        ResourceKeys.updateDBScheme(db);
+        ChunkKeys.updateDBScheme(db);
     }
 
     public String getCurrentNodeId() {
         return currentNodeId;
     }
 
-    public String getBucketName() {
-        return bucketName;
-    }
-
     public DateTime getCreationDate() {
         return creationDate;
+    }
+
+    public File getChunksDir() {
+        return chunksDir;
+    }
+
+    public int getMaxChunkSize() {
+        return maxChunkSize;
     }
 
 }
---
+++ src/main/java/com/chigix/resserver/error/UnwrappedExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.chigix.resserver.error;
+
+/**
+ *
+ * @author Richard Lea <chigix@zoho.com>
+ */
+public class UnwrappedExceptionHandler extends io.netty.handler.codec.http.router.UnwrappedExceptionHandler {
+
+}
