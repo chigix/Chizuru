917a23793a92362f26a20c0db1aee9fa51085ef3
Richard Lea <chigix@zoho.com>
2017-02-12 22:41:47 +0900

    new: dev: NoSuchUpload exception.
    Signed-off-by: Richard Lea <chigix@zoho.com>

---
+++ src/main/java/com/chigix/resserver/entity/error/NoSuchUpload.java
@@ -0,0 +1,21 @@
+package com.chigix.resserver.entity.error;
+
+/**
+ *
+ * @author Richard Lea <chigix@zoho.com>
+ */
+public class NoSuchUpload extends DaoException {
+
+    @Override
+    public String getMessage() {
+        return "The specified multipart upload does not exist. "
+                + "The upload ID might be invalid, or the multipart upload "
+                + "might have been aborted or completed.";
+    }
+
+    @Override
+    public String getCode() {
+        return "NoSuchUpload";
+    }
+
+}
