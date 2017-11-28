db8062f0ce4a2ba8c8f150e6361302c001e4f24e
RichardLea <chigix@zoho.com>
2017-11-28 11:35:24 +0900

    Refactor: share mapstruct configuration for Subresource
    Although actually, to remove UploadingSubresourceBeanMapper could also
    be taken into consideration because the only difference previously on
    fetching parent AmassedResource from two different database has been
    removed in previous commit.
    
    Signed-off-by: RichardLea <chigix@zoho.com>

--- src/main/java/com/chigix/resserver/mybatis/mapstruct/SubresourceBeanMapper.java
+++ src/main/java/com/chigix/resserver/mybatis/mapstruct/SubresourceBeanMapper.java
@@ -1,52 +1,59 @@
 package com.chigix.resserver.mybatis.mapstruct;
 
 import com.chigix.resserver.domain.error.NoSuchBucket;
+import com.chigix.resserver.domain.model.resource.ChunkedResource;
 import com.chigix.resserver.mybatis.bean.ChunkedResourceBean;
-import com.chigix.resserver.mybatis.dao.ResourceMapper;
 import com.chigix.resserver.mybatis.record.Subresource;
+import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
  * @author Richard Lea <chigix@zoho.com>
  */
 @Mapper(
-        componentModel = "spring",
-        uses = {BeanFactory.class,
-            ChunksAdapterMapper.class,
-            MetaDataMapper.class}
+        config = SubresourceBeanMapper.CentralConfig.class
 )
-public abstract class SubresourceBeanMapper {
+public interface SubresourceBeanMapper {
 
-    @Autowired
-    protected ResourceBeanMapper resourceBeanMapper;
+    @MapperConfig(
+            componentModel = "spring",
+            uses = {BeanFactory.class,
+                ChunksAdapterMapper.class,
+                MetaDataMapper.class}
+    )
+    public interface CentralConfig {
 
-    @Autowired
-    protected ResourceMapper resourceDao;
+        @Mappings({
+            @Mapping(target = "id", ignore = true)
+            ,@Mapping(target = "parentVersionId", ignore = true)
+            ,@Mapping(target = "type", expression = "java(com.chigix.resserver.mybatis.bean.ChunkedResourceBean.TYPE)")
+            ,@Mapping(source = "ETag", target = "etag")
+            ,@Mapping(source = "key", target = "indexInParent")
+            ,@Mapping(target = "rangeStartByte", ignore = true)
+            ,@Mapping(target = "rangeEndByte", ignore = true)
+            ,@Mapping(target = "rangeStart4byte", ignore = true)
+            ,@Mapping(target = "rangeEnd4byte", ignore = true)
+        })
+        Subresource toRecord(ChunkedResource bean) throws NoSuchBucket;
 
-    @Mappings({
-        @Mapping(target = "id", ignore = true)
-        ,@Mapping(target = "parentVersionId", ignore = true)
-        ,@Mapping(target = "type", expression = "java(com.chigix.resserver.mybatis.bean.ChunkedResourceBean.TYPE)")
-        ,@Mapping(source = "ETag", target = "etag")
-        ,@Mapping(source = "key", target = "indexInParent")
-        ,@Mapping(target = "rangeStartByte", ignore = true)
-        ,@Mapping(target = "rangeEndByte", ignore = true)
-        ,@Mapping(target = "rangeStart4byte", ignore = true)
-        ,@Mapping(target = "rangeEnd4byte", ignore = true)
-    })
-    abstract public Subresource toRecord(ChunkedResourceBean bean) throws NoSuchBucket;
+        @Mappings({
+            @Mapping(target = "metaData", constant = "<Metas></Metas>")
+            ,@Mapping(source = "etag", target = "ETag")
+            ,@Mapping(source = "versionId", target = "chunksAdapter")
+            ,@Mapping(target = "bucket", ignore = true)
+            ,@Mapping(target = "entityStatus", ignore = true)
+        })
+        ChunkedResourceBean fromRecord(final Subresource record);
+    }
 
-    @Mappings({
-        @Mapping(target = "metaData", constant = "<Metas></Metas>")
-        ,@Mapping(source = "etag", target = "ETag")
-        ,@Mapping(source = "versionId", target = "chunksAdapter")
-        ,@Mapping(target = "bucket", ignore = true)
-        ,@Mapping(target = "entityStatus", ignore = true)
-    })
-    abstract public ChunkedResourceBean fromRecord(final Subresource record);
+    @InheritConfiguration
+    Subresource toRecord(ChunkedResourceBean bean) throws NoSuchBucket;
+
+    @InheritConfiguration
+    ChunkedResourceBean fromRecord(final Subresource record);
 
 }
--- src/main/java/com/chigix/resserver/mybatis/mapstruct/UploadingSubresourceBeanMapper.java
+++ src/main/java/com/chigix/resserver/mybatis/mapstruct/UploadingSubresourceBeanMapper.java
@@ -4,47 +4,20 @@ import com.chigix.resserver.domain.model.resource.ChunkedResource;
 import com.chigix.resserver.domain.error.NoSuchBucket;
 import com.chigix.resserver.mybatis.bean.ChunkedResourceBean;
 import com.chigix.resserver.mybatis.record.Subresource;
+import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 /**
  *
  * @author Richard Lea <chigix@zoho.com>
  */
-@Mapper(
-        componentModel = "spring",
-        uses = {BeanFactory.class,
-            MetaDataMapper.class}
-)
-public abstract class UploadingSubresourceBeanMapper {
+@Mapper(config = SubresourceBeanMapper.CentralConfig.class)
+public interface UploadingSubresourceBeanMapper {
 
-    /**
-     * @TODO Refactoring upon mapstruct sharable configuration.
-     *
-     * @param record
-     * @return
-     */
-    @Mappings({
-        @Mapping(target = "metaData", constant = "<Metas></Metas>")
-        ,@Mapping(source = "etag", target = "ETag")
-        ,@Mapping(target = "chunksAdapter", ignore = true)
-        ,@Mapping(target = "bucket", ignore = true)
-        ,@Mapping(target = "entityStatus", ignore = true)
-    })
-    abstract public ChunkedResourceBean fromRecord(final Subresource record);
+    @InheritConfiguration
+    ChunkedResourceBean fromRecord(final Subresource record);
 
-    @Mappings({
-        @Mapping(target = "id", ignore = true)
-        ,@Mapping(target = "parentVersionId", ignore = true)
-        ,@Mapping(target = "type", expression = "java(com.chigix.resserver.mybatis.bean.ChunkedResourceBean.TYPE)")
-        ,@Mapping(source = "ETag", target = "etag")
-        ,@Mapping(source = "key", target = "indexInParent")
-        ,@Mapping(target = "rangeStartByte", ignore = true)
-        ,@Mapping(target = "rangeEndByte", ignore = true)
-        ,@Mapping(target = "rangeStart4byte", ignore = true)
-        ,@Mapping(target = "rangeEnd4byte", ignore = true)
-    })
-    abstract public Subresource toRecord(ChunkedResource bean) throws NoSuchBucket;
+    @InheritConfiguration
+    Subresource toRecord(ChunkedResource bean) throws NoSuchBucket;
 
 }
